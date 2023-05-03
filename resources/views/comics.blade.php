3ea0bfa0a4017f7e43a488978e839167ad10e4a0
Gaspare <Gas9220@users.noreply.github.com>
2023-05-03 17:26:51 +0200

    Jumbotron and features section refactor

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -25,36 +25,53 @@
 
 @extends('layouts.app')
 
-{{-- @section('page.main')
-    <section class="jumbo">
-    </section>
-    <section class="comics-area">
-        <div class="container">
-            <section class="comics-list">
-                <div class="series">
-                    <div class="primary-button">CURRENT SERIES</div>
-                </div>
-                <div>
-                    @foreach ($comics as $comic)
-                        <div class="comics-item">
-                            <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
-                            <h5>{{ $comic['title'] }}</h5>
-                        </div>
-                    @endforeach
-                </div>
-                <div class="primary-button">LOAD MORE...</div>
-            </section>
+@section('page.main')
+    {{-- Jumbo --}}
+    <section class="jumbo"></section>
+    {{-- /Jumbo --}}
 
+    {{-- Comics --}}
+    <section class="comics"></section>
+    {{-- ⁄Comics --}}
+
+    {{-- Features --}}
+    <section class="features">
+        <div class="container d-flex justify-content-center flex-wrap p-4">
+            @foreach ($features as $feature)
+                <div class="feature d-flex align-items-center p-2">
+                    <img src="{{ Vite::asset($feature['icon']) }}" alt="{{ $feature['text'] }}">
+                    <h4 class="text-white ms-2 fs-5">{{ $feature['text'] }}</h4>
+                </div>
+            @endforeach
         </div>
-        <section class="features">
-            <div class="container">
-                @foreach ($features as $feature)
-                <div class="feature">
-                        <img src="{{ Vite::asset($feature['icon']) }}" alt="{{ $feature['text'] }}">
-                        <h4>{{ $feature['text'] }}</h4>
-                    </div> 
+    </section>
+    {{-- /Features --}}
+@endsection
+
+
+
+
+
+{{-- 
+<section class="comics-area">
+    <div class="container">
+        <section class="comics-list">
+            <div class="series">
+                <div class="primary-button">CURRENT SERIES</div>
+            </div>
+            <div>
+                @foreach ($comics as $comic)
+                    <div class="comics-item">
+                        <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                        <h5>{{ $comic['title'] }}</h5>
+                    </div>
                 @endforeach
             </div>
+            <div class="primary-button">LOAD MORE...</div>
         </section>
+
+    </div>
+    <section class="features">
+
     </section>
-@endsection --}}
+</section> --}}
