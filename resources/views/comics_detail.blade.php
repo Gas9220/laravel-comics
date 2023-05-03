84118c5a34babf39af23a1fc82d7b2b88f671b46
Gaspare <Gas9220@users.noreply.github.com>
2023-05-03 22:15:23 +0200

    Detail comics cover

--- resources/views/comics_detail.blade.php
+++ resources/views/comics_detail.blade.php
@@ -9,5 +9,10 @@ Comics Detail
 @section('page.main')
 <section class="jumbo"></section>
 {{-- TODO UI --}}
+<section class="comics-cover">
+    <div class="container">
+        <img class="shift-80" src="{{$comics['thumb']}}" alt="cover">
+    </div>
+</section>
 <h1>{{ $comics['title'] }}</h1>
 @endsection
